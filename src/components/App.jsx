7dd0dc03a404671cc85175e6846a913ca8f580f3
Daniyal Aamir <daamir89@gmail.com>
2024-06-21 16:37:22 -0400

    add local storage to persist and retreive items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import BackgroundHeading from "./BackgroundHeading"
 import Footer from "./Footer"
 import Header from "./Header"
@@ -7,7 +7,9 @@ import Sidebar from "./Sidebar"
 import { INITIAL_ITEMS } from "../lib/constants"
 
 function App() {
-  const [items, setItems] = useState(INITIAL_ITEMS)
+  const [items, setItems] = useState(
+    () => JSON.parse(localStorage.getItem("items")) || INITIAL_ITEMS
+  )
 
   const handleAddItem = (newItemText) => {
     const newItem = {
@@ -55,6 +57,10 @@ function App() {
     })
     setItems(newItems)
   }
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items))
+  }, [items])
 
   return (
     <>
